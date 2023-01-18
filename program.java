f328f76b6b62385780bbcf04337ca580d2d00a0d
CharanChery <95143627+CharanChery@users.noreply.github.com>
2023-01-18 05:40:35 +0000

    Main pull request

---
+++ program.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class program
+{
+    public static void main(String arg[])
+    {
+        Scanner s = new Scanner(System.in);
+        int a = s.nextInt();
+        System.out.println(a+" "+"Hello");
+    }
+}
